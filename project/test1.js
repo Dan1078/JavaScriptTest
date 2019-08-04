9287744a5546559be05c7218e0ea83459b4f5e5a
D <example@example.com>
2019-08-04 22:36:22 +0300

    first commit

---
+++ project/test1.js
@@ -0,0 +1,50 @@
+var leftBoardWidth = 1;
+const pi = 3.14;
+{
+    let second = 2;
+}
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+console.log(4/0);
+console.log('string'*9); 
+
+
+obj = {
+    name: "Jon",
+    age: 25,
+    isMarried: false
+}
+
+console.log(obj["name"]);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[0]);
+
+// alert("Hello!");
+// confirm("Are you here?");
+// var answer = +prompt("18???", "18");
+// console.log(typeof(answer));
+// // console.log('arr' + 'arr2');
+// // console.log(8 + "");
+var incr = 10;
+var decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(typeof(decr));
+console.log(typeof(9));
+
+console.log(incr++==10);
+console.log(decr-- === 9);
+
+var isCheked = true;
+var isClosed = true;
+
+console.log(isCheked && !isClosed);
